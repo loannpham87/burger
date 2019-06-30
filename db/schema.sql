d2f58a124ad8a2f5aa47bd5e8dc9e19b220ff664
loannpham87 <loannpham87@gmail.com>
2019-06-29 23:37:44 -0700

    Need to fix eatburger button

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+-- Drops the burger_db if it already exists --
+DROP DATABASE IF EXISTS burger_db;
+
+-- Create the database burger_db and specified it for use.
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+-- Create the table burgers.
+CREATE TABLE burgers (
+  id int NOT NULL AUTO_INCREMENT,
+  burger varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger) VALUES ("California Burger"),
+("Veggie Burger"),
+("Meat Lovers Burger"),
+("Breakfast Burger"),
+("All-American Burger"),
+("The Whole Damn Farm Burger");
